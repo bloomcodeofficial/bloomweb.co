1abdd80be6d49eaf2eeb1e40c17cbf5a5e03e1b6
niklashans <104555311+niklashans@users.noreply.github.com>
2023-01-15 19:18:31 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
-import { greetUser } from '$utils/greet';
+// Get the navbar element
+const navbar = document.querySelector('[bw-element="navbar"]');
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+// Set the initial scroll position
+let lastScroll = 0;
+
+// Listen for the scroll event
+window.addEventListener('scroll', () => {
+  // Get the current scroll position
+  const currentScroll = window.pageYOffset;
+
+  // Check if the scroll position is at the top
+  if (window.pageYOffset === 0) {
+    // Remove the class from the navbar
+    navbar.classList.remove('nav-up');
+    navbar.classList.remove('nav-down');
+    navbar.classList.remove('nav-fixed');
+
+    // Update the scroll position
+    lastScroll = currentScroll;
+  } else if (window.pageYOffset > 500) {
+    navbar.classList.add('nav-up');
+    navbar.classList.add('nav-fixed')
+  }
+
+  // Check if the user has scrolled up
+  if (currentScroll < lastScroll && window.pageYOffset > 500) {
+    // Add the class to the navbar
+    navbar.classList.add('nav-down');
+  } else if (currentScroll > lastScroll && window.pageYOffset > 500) {
+    // Remove the class from the navbar when the user scrolls down
+    navbar.classList.remove('nav-down');
+  }
+  // Update the scroll position
+  lastScroll = currentScroll;
 });
