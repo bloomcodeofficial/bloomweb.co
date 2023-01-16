e207b88de2e1675e3c2a108174e3ac9d1461181d
niklashans <104555311+niklashans@users.noreply.github.com>
2023-01-16 11:19:04 +0100

    Added navigation to swiper.js configurations

--- src/swiper-slider.ts
+++ src/swiper-slider.ts
@@ -1,13 +1,13 @@
-$(".slider_component").each(function (index) {
+$('.slider_component').each(function (index) {
   let loopMode = false;
-  if ($(this).attr("loop-mode") === "true") {
+  if ($(this).attr('loop-mode') === 'true') {
     loopMode = true;
   }
   let sliderDuration = 300;
-  if ($(this).attr("slider-duration") !== undefined) {
-    sliderDuration = +$(this).attr("slider-duration");
+  if ($(this).attr('slider-duration') !== undefined) {
+    sliderDuration = +$(this).attr('slider-duration');
   }
-  const swiper = new Swiper($(this).find(".swiper")[0], {
+  const swiper = new Swiper($(this).find('.swiper')[0], {
     speed: sliderDuration,
     loop: loopMode,
     autoHeight: false,
@@ -17,41 +17,45 @@ $(".slider_component").each(function (index) {
     freeMode: false,
     slideToClickedSlide: false,
     slidesPerView: 1,
-    spaceBetween: "4%",
+    spaceBetween: '4%',
     rewind: false,
     mousewheel: {
-      forceToAxis: true
+      forceToAxis: true,
     },
     keyboard: {
       enabled: true,
-      onlyInViewport: true
+      onlyInViewport: true,
     },
     breakpoints: {
       // mobile landscape
       480: {
         slidesPerView: 1,
-        spaceBetween: "4%"
+        spaceBetween: '4%',
       },
       // tablet
       768: {
         slidesPerView: 2,
-        spaceBetween: "4%"
+        spaceBetween: '4%',
       },
       // desktop
       992: {
         slidesPerView: 3,
-        spaceBetween: "2%"
-      }
+        spaceBetween: '2%',
+      },
     },
     pagination: {
-      el: $(this).find(".swiper-bullet-wrapper")[0],
-      bulletActiveClass: "is-active",
-      bulletClass: "swiper-bullet",
-      bulletElement: "button",
-      clickable: true
+      el: $(this).find('.swiper-bullet-wrapper')[0],
+      bulletActiveClass: 'is-active',
+      bulletClass: 'swiper-bullet',
+      bulletElement: 'button',
+      clickable: true,
     },
-    slideActiveClass: "is-active",
-    slideDuplicateActiveClass: "is-active"
+    navigation: {
+      nextEl: $(this).find('.swiper-next')[0],
+      prevEl: $(this).find('.swiper-prev')[0],
+      disabledClass: 'is-disabled',
+    },
+    slideActiveClass: 'is-active',
+    slideDuplicateActiveClass: 'is-active',
   });
 });
-
