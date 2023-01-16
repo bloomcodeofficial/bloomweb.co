4dd3330f7fc0a0ab066de693ed1b9b259e895d0d
niklashans <104555311+niklashans@users.noreply.github.com>
2023-01-16 10:57:31 +0100

    Updated Swiper code from T ricks

--- src/swiper-slider.ts
+++ src/swiper-slider.ts
@@ -1,68 +1,57 @@
-$('.slider_component').each(function (index) {
+$(".slider_component").each(function (index) {
   let loopMode = false;
-  if ($(this).attr('loop-mode') === 'true') {
+  if ($(this).attr("loop-mode") === "true") {
     loopMode = true;
   }
   let sliderDuration = 300;
-  if ($(this).attr('slider-duration') !== undefined) {
-    sliderDuration = +$(this).attr('slider-duration');
+  if ($(this).attr("slider-duration") !== undefined) {
+    sliderDuration = +$(this).attr("slider-duration");
   }
-
-  const swiper = new Swiper($(this).find('.swiper')[0], {
-    slidesPerView: '1',
-    loop: false,
-    speed: 700,
-    keyboard: true,
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    speed: sliderDuration,
+    loop: loopMode,
+    autoHeight: false,
     initialSlide: 1,
+    centeredSlides: loopMode,
+    followFinger: true,
+    freeMode: false,
+    slideToClickedSlide: false,
+    slidesPerView: 1,
+    spaceBetween: "4%",
+    rewind: false,
     mousewheel: {
-      forceToAxis: true,
+      forceToAxis: true
     },
-    spaceBetween: '5%',
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    },
     breakpoints: {
-      // mobile portrait
-      320: {
-        slidesPerView: 1.3,
-        spaceBetween: '4%',
-        centeredSlides: true,
-      },
-
       // mobile landscape
       480: {
-        slidesPerView: 1.3,
-        spaceBetween: '4%',
-        centeredSlides: true,
+        slidesPerView: 1,
+        spaceBetween: "4%"
       },
       // tablet
       768: {
-        slidesPerView: 1.4,
-        spaceBetween: '3%',
-        centeredSlides: true,
+        slidesPerView: 2,
+        spaceBetween: "4%"
       },
       // desktop
       992: {
         slidesPerView: 3,
-        spaceBetween: '2%',
-      },
+        spaceBetween: "2%"
+      }
     },
     pagination: {
-      el: $(this).find('.swiper-bullet-wrapper')[0],
-      bulletActiveClass: 'is-active',
-      bulletClass: 'swiper-bullet',
-      bulletElement: 'button',
-      clickable: true,
+      el: $(this).find(".swiper-bullet-wrapper")[0],
+      bulletActiveClass: "is-active",
+      bulletClass: "swiper-bullet",
+      bulletElement: "button",
+      clickable: true
     },
-    navigation: {
-      nextEl: $(this).find('.swiper-next')[0],
-      prevEl: $(this).find('.swiper-prev')[0],
-      disabledClass: 'is-disabled',
-    },
-    scrollbar: {
-      el: $(this).find('.swiper-drag-wrapper')[0],
-      draggable: true,
-      dragClass: 'swiper-drag',
-      snapOnRelease: true,
-    },
-    slideActiveClass: 'is-active',
-    slideDuplicateActiveClass: 'is-active',
+    slideActiveClass: "is-active",
+    slideDuplicateActiveClass: "is-active"
   });
 });
+
